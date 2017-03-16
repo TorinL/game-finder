721ac6f987d64e21c3b789e61b1bccc0a255ed6c
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-03-16 11:55:21 -0700

    practice prersentation

--- main.js
+++ main.js
@@ -1,36 +1,56 @@
-console.log('sanity check');
+const apiKey = '6add1d63e9fac30d8ed40467212d1d35a332d65e';
+const apiProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
+const baseUrl = 'http://www.giantbomb.com/api/search/?api_key=';
 
+function getGameData(searchString) {
+  let apiUrl = `${apiProxy}${baseUrl}${apiKey}&format=json&query=${searchString}&resources=game`;
 
-var apiKey = '6add1d63e9fac30d8ed40467212d1d35a332d65e';
-var apiProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
-var baseUrl = 'http://www.giantbomb.com/api/search/?api_key=';
+  return $.get(apiUrl)
+}
 
+function buildCards(cardData) {
+  console.log(cardData)
+  for (var i = 0; i < cardData.results.length; i++) {
+    let $gameResults = $('<div class="game-results col-xs-3">')
+    let $gameImage = $('<div class="game-image">')
+    let $anchor = $('<a href="page.html"></a>')
+    let $noResize = $(`<img id="${i}" class="no-resize" src="${cardData.results[i].image.medium_url}" />`)
+
+    $gameResults.append($gameImage)
+    $gameImage.append($anchor)
+    $anchor.append($noResize)
+
+    $('.blah-city').append($gameResults);
+  }
+}
 
 $('form').submit(function(e) {
-  var searchString = $('.form-control').val();
-  var apiUrl = apiProxy + baseUrl + apiKey + '&format=json&query=' + searchString + '&resources=game';
-  console.log(apiUrl);
-  $.ajax({
-    url: apiUrl,
-    method: 'GET'
-  }).then(function(result){
-
-    for (var i = 0; i <= 8; i++) {
-      console.log(result.results[i])
-      $('#'+ i).attr('src',result.results[i].image.medium_url);
-    } if ($('#'+ i).attr('src',result.results[i].image.medium_url)===null){
-      return hideDiv
-    }
-  }).catch(function (error){
-    console.log('Error: ', error)
-  })
   e.preventDefault()
+
+  let searchString = $('.form-control').val();
+
+  getGameData(searchString)
+    .then(function(cardData){
+      buildCards(cardData);
+    })
+    .catch(function (error){
+      console.log('Error: ', error)
+    })
 });
 
+//build the function to bring in all the infor for a game
+//buildthe jQuery call to run it on click of image
 
-
-function unhideDiv(element){
-  $('.game-results').removeClass('hidden')
-};
-
-$('#search-it-brah').on('click', unhideDiv)
+// function callInfo() {
+//   $.ajax({
+//     url: 'https://galvanize-student-apis.herokuapp.com/gcommerce/products',
+//     method: 'GET'
+//     }).then(function (response) {
+//     console.log(response.length)
+//     console.log(response)
+//     }).catch(function (error) {
+//     console.log('error', error);
+//   })
+// }
+// $('.form-control').on('click', )
+// $('.blah-city').on('click', )
