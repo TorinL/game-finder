f05ea2b995a8593e70d6d97a06e27fdf6c6d64b6
torinlongaker@gmail.com <torinlongaker@gmail.com>
2017-03-17 10:43:02 -0700

    present

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 const apiKey = '6add1d63e9fac30d8ed40467212d1d35a332d65e';
-const apiProxy = 'http://galvanize-cors-proxy.herokuapp.com/';
+const apiProxy = 'https://galvanize-cors-proxy.herokuapp.com/';
 const baseUrl = 'http://www.giantbomb.com/api/search/?api_key=';
 
 function getGameData(searchString) {
@@ -9,25 +9,25 @@ function getGameData(searchString) {
 }
 
 function buildCards(cardData) {
-  $('.blah-city').empty()
+  $('#game-pictures').empty()
+
 
   for (var i = 0; i < cardData.results.length; i++) {
-    let $gameResults = $('<div class="game-results col-xs-3">')
+    let $gameResults = $(`<div id="${cardData.results[i].id}" class="game-results col-xs-3">`)
+    $gameResults.on('click', 'img', function(){
+      let gameId = $(this).attr('id')
+      let gameInfo = cardData.results[gameId].description
+      $('#game-info').empty()
+      $('#game-info').append(gameInfo)
+    })
     let $gameImage = $('<div class="game-image">')
     let $noResize = $(`<img id="${i}" class="no-resize" src="${cardData.results[i].image.medium_url}" />`)
 
     $gameResults.append($gameImage)
     $gameImage.append($noResize)
 
-    $('.blah-city').append($gameResults);
+    $('.game-pictures').append($gameResults);
   }
-  $('.blah-city').on('click', 'img', function(){
-    $('blah-town').empty()
-    let gameId = $(this).attr('id')
-    let gameInfo = cardData.results[gameId].description
-
-    $('.blah-town').append(gameInfo)
-  })
 }
 
 
